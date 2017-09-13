aeb0fa0d41026370e3344732f2ea95797b96fc45
Paajule <paajule@localhost.localdomain>
2017-09-13 10:19:23 +0200

    mkdir list_2

---
+++ src/app/list_2/list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+import { IonicPageModule } from 'ionic-angular';
+
+import { SharedModule } from '../shared/shared.module';
+import { ListPage } from './list';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    IonicPageModule.forChild(ListPage)
+  ],
+  declarations: [
+    ListPage
+  ],
+  entryComponents: [
+    ListPage
+  ]
+})
+export class ListPageModule { }
---
+++ src/app/list_2/list.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { NavController, IonicPage, AlertController } from 'ionic-angular';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import 'rxjs/add/operator/map';
+
+import { DataService } from '../core/data.service';
+import { AuthService } from '../core/auth.service';
+import { UserModel } from '../core/user.model'
+import { PetModel } from '../core/pet.model'
+import { Config } from '../env.constants'
+
+@IonicPage()
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+
+  public pets: PetModel[] = [
+    {
+      name: "Fido",
+      animal: "pes",
+      age: 10
+    }
+  ];
+  public userProfile: UserModel;
+  public uid: string = "";
+  public pet: any = {
+    "name": "",
+    "animal": "",
+    "age": 0
+  }
+  
+
+  constructor(
+    public navCtrl: NavController, 
+    public db: DataService,
+    public authService: AuthService,
+    private alertCtrl: AlertController
+  ) {
+    
+  }
+
+  ionViewDidLoad() {
+    this.authService.getFullProfile().subscribe((user) => {
+      this.userProfile = user;
+      this.uid = user.uid;
+    });
+    /*this.tasks = this.db.listAll(Config.firebase_tables.Tasks, {
+      orderByChild: "deadline"
+    }).map(
+      tasks => tasks.filter(
+        task => task.user == this.uid
+      )
+    ) as FirebaseListObservable<any[]>;*/
+  }
+
+  saveNnext() {
+    this.pets.push(this.pet);
+    this.pet = {};
+    //this.navCtrl.push("") 
+  }
+  
+  }
